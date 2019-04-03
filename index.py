47d69b43d60624b25ceff58c19a0f59869db042f
cristiangabbi <49098505+cristiangabbi@users.noreply.github.com>
2019-04-04 00:50:06 +0200

    Add files via upload
    Parser done.
    index.py adds stuff to the database(almost)

--- index.py
+++ index.py
@@ -1,11 +1,17 @@
+#!/usr/bin/python3
 from whoosh.index import create_in, open_dir
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
 import os
 
-class index():
-    def __init__(self):
+import xml.sax
+from saxParser import DBLPHandler
 
+import sqlite3
+from sqlite3 import Error
+
+'''class index():
+    def __init__(self):
         self.schema = Schema(title = TEXT(stored=TRUE),
                         author = TEXT(stored=TRUE),
                         year = NUMERIC(stored=TRUE)
@@ -15,6 +21,84 @@ class index():
 
         if not os.path.exists('indice'):
             os.mkdir('indice')
-        create_in('indice', schema)
+        create_in('indice', schema)'''
 
-    def populateIndex(self):
+
+
+def extractDataWithParser(path):
+    print("Parsing {}...".format(path))
+    parser = xml.sax.make_parser()
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+
+    h = DBLPHandler()
+    parser.setContentHandler(h)
+
+    try:
+        parser.parse(path)
+    except FileNotFoundError:
+        print("Errore! " + e)
+        return False
+
+    return h.data
+
+
+
+'''Connessione con db, restituisce False se la connessione fallisce.'''
+def connectToDb():
+    try:
+        conn = sqlite3.connect('../db/dbGAvI')
+        return conn
+    except Error as e:
+        print(e)
+
+    return False
+
+
+def getData(dic, str):
+    if dic.get(str):
+        if str == 'author':
+            app = ''
+            for a in dic[str]:
+                app+= a + ", "
+            return app[:-2]
+        else:
+            return dic[str]
+    else:
+        return ''
+
+
+
+
+'''inserimento di roba nel db.'''
+def main():
+    print("doing stuff...")
+
+    data = extractDataWithParser("../dblp.xml")
+    conn = connectToDb()
+    c = conn.cursor()
+
+    for dic in data:
+        txt = "INSERT INTO article VALUES('{}','{}','{}',{},'{}','{}','{}','{}','{}')".format( \
+                getData(dic,'key'), \
+                getData(dic,'author'), \
+                getData(dic,'title'), \
+                getData(dic,'year'), \
+                getData(dic,'url'), \
+                getData(dic,'ee'), \
+                getData(dic,'journal'), \
+                getData(dic,'booktitle'), \
+                getData(dic,'crossref'))
+        try:
+            c.execute(txt)
+        except Error as e:
+            print("Error! {} \n Query not executed: {}".format(e,txt))
+            return
+
+    conn.commit()
+    conn.close()
+    print('Done.')
+
+
+
+if __name__ == '__main__':
+    main()
--- saxParser.py
+++ saxParser.py
@@ -1,59 +1,87 @@
 '''SAX handler per il file XML.'''
 import xml.sax
 
-
-
 class DBLPHandler(xml.sax.ContentHandler):
     def __init__(self):
+        self.__fields = ["title",
+                            "year",
+                            "key",
+                            "publisher",
+                            "crossref",
+                            "url",
+                            "ee",
+                            "journal",
+                            "booktitle"] #author va gestito separatamente
+
         self.CurrentData = ''
+        self.key = ''
         self.title = ''
-        self.author = ''
+        self.author = []
         self.year = ''
         self.publisher = ''
-        self.key = ''
         self.crossref = ''
         self.url = ''
+        self.journal = ''
+        self.ee = ''
+        self.booktitle = ''
+
+        self.__AllData = []
+
+
+    '''Eliminazione dei caratteri che disturbano l'inserimento nel db'''
+    def __correctString(self, txt):
+        app = txt.replace(chr(39),'') #carattere '
+        txt = app.replace(chr(34),"") #carattere "
+        return txt
+
 
     '''Chiamato quando viene letto un nuovo articolo.'''
     def startElement(self, tag, attr):
         self.CurrentData = tag
-        if tag == 'article':
-            print('/************************ Article ****************************/')
-            key = attr['key']
-            mdate = attr['mdate']
-            print('key: {}   -   mdate: {}'.format(key, mdate))
+
+        if tag == 'article' or tag == 'inproceedings':
+            self.key = attr['key']
+
+
 
     '''Chiamato alla fine di un articolo.'''
     def endElement(self, tag):
-        if self.CurrentData == "author":
-            print ("Author:", self.author)
-        elif self.CurrentData == "title":
-            print ("Title:", self.title)
-        elif self.CurrentData == "year":
-            print ("Year:", self.year)
-        elif self.CurrentData == "publisher":
-            print ("Publisher:", self.publisher)
-        elif self.CurrentData == "crossref":
-            print ("Crossref:", self.crossref)
-        elif self.CurrentData == "url":
-            print ("Url:", self.url)
+        if tag == 'article' or tag == 'inproceedings':
+
+            '''creazione del dict con le info prese dall'articolo'''
+            appoggio = {x:getattr(self,x)  \
+                for x in self.__fields+['author']  \
+                if getattr(self,x) != ''}
+
+            self.__AllData.append(appoggio)
+
+            '''Svuoto i vari campi.'''
+            self.author = []
+            for x in self.__fields:
+                setattr(self,x,'')
+
         self.CurrentData = ""
+
+
+
+    def endElement(self, tag):
+        print('Done parsing')
+
+        
 
     '''Lettura di un articolo.'''
     def characters(self, content):
-        if self.CurrentData == "author":
-            self.author = content
-        elif self.CurrentData == "title":
-            self.title = content
-        elif self.CurrentData == "year":
-            self.year = content
-        elif self.CurrentData == "publisher":
-            self.publisher = content
-        elif self.CurrentData == "crossref":
-            self.crossref = content
-        elif self.CurrentData == "url":
-            self.url = content
+        if self.CurrentData in self.__fields:
+            setattr(self,self.CurrentData,self.__correctString(content))
 
+        elif self.CurrentData == 'author':
+            self.author.append(self.__correctString(content))
+
+
+    '''Restituzione della lista.'''
+    @property
+    def data(self):
+        return self.__AllData
 
 '''# create an XMLReader
    parser = xml.sax.make_parser()
